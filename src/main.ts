49dc6f086fcb5209ff6b8b8d5de0d6892d65ad5e
rasul <shiroeqky@gmail.com>
2022-07-12 22:24:30 +0500

    feat: swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const root = dirname(__dirname);
+  const api = await readFile(join(root, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(api);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
